98ff5ea06edbf66118b65289b23c975c177f5b4c
Animesh Sharma <animesh1@live.com>
2021-06-09 04:21:54 +0530

    vercel test

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -3,11 +3,11 @@ import Features from './Features';
 import BannerCTA from './BannerCTA';
 
 export default () => {
-  
+
   return(
     <div className="mainSectionDiv" style={{"background-image": "url('/swoosh.png')"}}>
       <div>
-        <BannerCTA header="Aim High With Brilliant Writing" buttonName="Upgrade to Grammarly Premium" />
+        <BannerCTA header="Aim High With Brilliant Writing." buttonName="Upgrade to Grammarly Premium" />
         <Features />
       </div>
     </div>
