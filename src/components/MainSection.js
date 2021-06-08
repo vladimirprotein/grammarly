f8d30aff30b17ca73b864cb9e8531816f4a14978
Animesh Sharma <animesh1@live.com>
2021-06-09 05:17:08 +0530

    added progress bar

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -7,7 +7,7 @@ export default () => {
   return(
     <div className="mainSectionDiv" style={{"background-image": "url('/swoosh.png')"}}>
       <div>
-        <BannerCTA header="Aim High With Brilliant Writing." buttonName="Upgrade to Grammarly Premium" />
+        <BannerCTA header="Aim High With Brilliant Writing" buttonName="Upgrade to Grammarly Premium" />
         <Features />
       </div>
     </div>
--- src/components/StatCard.js
+++ src/components/StatCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default (props) => {
   return(
     <div class="statCardDiv">
-      {props.text}
+      <div>
+        <CircularProgressbar styles={buildStyles({
+            textColor: "black",
+            pathColor: "#11a683",
+            trailColor: "#e6e6e8"
+          })} value={props.per} text={props.per + "%"}
+        />
+      </div>
+      <p>{`${props.per}% of Grammarly users ${props.text}`}</p>
     </div>
 
   );
